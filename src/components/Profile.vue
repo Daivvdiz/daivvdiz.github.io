3ea164e560ac0da5d035b9fa09b833cf6901c61c
David Diaz <daivvdiz@gmail.com>
2024-07-16 13:28:24 -0500

    Config Profile Component for Mobile View

--- src/components/Profile.vue
+++ src/components/Profile.vue
@@ -43,4 +43,33 @@
         letter-spacing: 1px;
     }
 }
+
+@media screen and (max-width: 1024px){
+    .profileContainer {
+        display: flex;
+        flex-direction: column;
+        justify-content: center;
+        align-items: center;
+        margin-top: 5vh;
+        padding: 2vh 0;
+        max-height: 500px;
+    }
+
+    .photoContainer {
+        margin-top: 1rem;
+        margin-bottom: .5rem;
+        border-radius: 50%;
+        filter: drop-shadow(0 0 0.4rem #faebd740);
+    }
+
+    .photoContainer img {
+        max-height: 120px;
+        border-radius: 50%;
+    }
+
+    .messageContainer h3 {
+        font-size: .8rem;
+        letter-spacing: 1px;
+    }
+}
 </style>

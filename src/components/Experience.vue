991abe8939e4ea84b3db525bb5b76e4f6a066522
David Diaz <daivvdiz@gmail.com>
2024-07-16 13:40:18 -0500

    Add Responsive Mobile View to Experience Component

--- src/components/Experience.vue
+++ src/components/Experience.vue
@@ -1,13 +1,15 @@
 <template>
     <div class="carouselContainer">
-        <div class="iconItem">
-            <a href="https://emcocables.co/" target="_blank"><img src="@/assets/icons/Emcocables.png" alt="Emcocables"></a>
-        </div>
-        <div class="iconItem">
-            <a href="https://www.usbbog.edu.co/" target="_blank"><img src="@/assets/icons/Universidad.png" alt="Universidad"></a>
-        </div>
-        <div class="iconItem">
-            <a href="https://www.sena.edu.co/es-co/Paginas/default.aspx" target="_blank"><img src="@/assets/icons/Sena.png" alt="SENA"></a>
+        <div class="iconContainer">
+            <div class="iconItem">
+                <a href="https://emcocables.co/" target="_blank"><img src="@/assets/icons/Emcocables.png" alt="Emcocables"></a>
+            </div>
+            <div class="iconItem">
+                <a href="https://www.usbbog.edu.co/" target="_blank"><img src="@/assets/icons/Universidad.png" alt="Universidad"></a>
+            </div>
+            <div class="iconItem">
+                <a href="https://www.sena.edu.co/es-co/Paginas/default.aspx" target="_blank"><img src="@/assets/icons/Sena.png" alt="SENA"></a>
+            </div>
         </div>
     </div>
 </template>
@@ -36,5 +38,42 @@
     }
 }
 
+@media screen and (max-width: 1024px){
+    .carouselContainer {
+        display: flex;
+        width: 100%;
+        align-items: center;
+        padding: 5rem 0;
+    }
+
+    .iconContainer {
+        display: flex;
+        width: 100%;
+        overflow-x: scroll;
+    }
+
+    .iconItem {
+        display: flex;
+        width: 400px;
+        margin: 0 3rem;
+        justify-content: center;
+        align-items: center;
+    }
+
+    .iconContainer a {
+        display: flex;
+        height: 100%;
+        margin: 0 5rem;
+    }
+
+    .carouselContainer a:hover {
+        filter: invert(.4);
+    }
+
+    .carouselContainer a img {
+        max-height: 80px;
+        margin: 0 2rem;
+    }
+}
 
 </style>
